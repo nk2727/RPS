9ffbcdf7afa10bc693e223a484cc531e2aa39200
Nicole <nicole@njcdtl01lt3981.itservices.sbc.com>
2016-03-22 17:01:48 -0400

    Initial Commit

--- RPS/PlayViewController.swift
+++ RPS/PlayViewController.swift
@@ -8,8 +8,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class PlayViewController: UIViewController {
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +20,8 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBOutlet weak var playRock: UIButton!
+    
 }
 
---
+++ RPS/ResultsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ResultsViewController.swift
+//  RPS
+//
+//  Created by Nicole on 3/22/16.
+//  Copyright © 2016 Nicole. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ResultsViewController: UIViewController {
+    
+    @IBOutlet weak var resultsLable: UILabel!
+    @IBOutlet weak var resultsImage: UIImage!
+    @IBOutlet weak var playAgain: UIButton!
+    
+    
+}
