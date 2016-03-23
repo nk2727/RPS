d7252f4035a683f8946ee9aff51eff0d43ecb35d
Nicole Kowalski <nickow@nicoles-mac.local>
2016-03-22 22:50:40 -0400

    Implement view transition with Code Only

--- RPS/PlayViewController.swift
+++ RPS/PlayViewController.swift
@@ -10,18 +10,47 @@ import UIKit
 
 class PlayViewController: UIViewController {
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+  
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
     @IBOutlet weak var playRock: UIButton!
+    @IBOutlet weak var letsplay: UILabel!
     
-}
+    
+    func randomShape() -> String {
+        
+        let randomNum = 1 + arc4random() % 3
+        var randomShape: String!
+        
+        switch randomNum {
+        case 1 :
+            randomShape="Rock"
+        case 2:
+            randomShape="Paper"
+        default:
+            randomShape="Scissors"
+        }
+        return(randomShape)
+    }
 
+    @IBAction func pickRock(sender: UIButton) {
+       
+        //
+        // get a ResultsViewController
+        // UserShape, appShape are properties
+        //
+        var controller: ResultsViewController
+        controller = self.storyboard?.instantiateViewControllerWithIdentifier("ResultsViewController") as! ResultsViewController
+        controller.userShape = "Rock"
+        controller.appShape = randomShape()
+
+       self.presentViewController(controller, animated: true, completion: nil)
+    }
+}
--- RPS/ResultsViewController.swift
+++ RPS/ResultsViewController.swift
@@ -11,9 +11,63 @@ import UIKit
 
 class ResultsViewController: UIViewController {
     
-    @IBOutlet weak var resultsLable: UILabel!
-    @IBOutlet weak var resultsImage: UIImage!
+
+    @IBOutlet weak var resultsImage: UIImageView!
+    @IBOutlet weak var resultsLabel: UILabel!
     @IBOutlet weak var playAgain: UIButton!
     
+    var userShape: String!
+    var appShape: String!
     
+    override func viewDidAppear(animated: Bool) {
+        
+        switch userShape {
+        case "Rock" :
+
+            if appShape == "Rock" {
+                resultsLabel.text = "App picked Rock - It's a tie"
+                //resultsImage.image=UIImage(named: "          }
+                
+            } else if appShape == "Paper" {
+                resultsLabel.text = "App picked Paper - you lose"
+                
+            } else {
+                resultsLabel.text = "App picked Scissor - you win"
+                
+            }
+            
+        case "Paper" :
+            if appShape == "Rock" {
+                resultsLabel.text = "App picked \"(appShape - you lose"
+                //resultsImage.image=UIImage(named: "          }
+                
+            } else if appShape == "Paper" {
+                resultsLabel.text = "App picked \"(appShape - It's tie"
+                
+            } else {
+                resultsLabel.text = "App picked \"(appShape - you win"
+            }
+        default:
+            //
+            // useshape is Scissors
+            //
+    
+            if appShape == "Rock" {
+                resultsLabel.text = "App picked \"(appShape) - you lose"
+                //resultsImage.image=UIImage(named: "          }
+                
+            } else if appShape == "Paper" {
+                resultsLabel.text = "App picked \"(appShape) - you win"
+                
+            } else {
+                resultsLabel.text = "App picked \"(appShape) - it's tie"
+            }
+    
+        }
+        
+    }
+    
+    @IBAction func playAgain(sender: UIButton) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
 }
